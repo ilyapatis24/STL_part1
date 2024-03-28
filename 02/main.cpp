4972daaf06ea717fa388dcffc848d2b4c7ba8560
ilyapatis24 <ilyapatis24@mail.ru>
2024-03-29 02:42:03 +0300

    second mission completed

---
+++ 02/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <set>
+
+int main(int argc, char** argv) {
+    setlocale(LC_ALL, "Russian");
+    int count = 0;
+    std::cout << "[IN]: ";
+    std::cin >> count;
+	std::set<int, std::greater<int>> setOfValues;
+
+	for (size_t i {0}, v {0}; i < count; ++i)
+	{
+		std::cin >> v;
+		setOfValues.insert(v);
+	}
+
+	std::cout << "[OUT]:" << std::endl;
+	for (const auto& v : setOfValues)
+	{
+		std::cout << v << std::endl;
+	}
+    return 0;
+}
