dcdab282494574e36ad0686c81a9d408d385f89c
ilyapatis24 <ilyapatis24@mail.ru>
2024-03-28 23:05:47 +0300

    initial commit

---
+++ 01/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+
+int main(int argc, char** argv) {
+    setlocale(LC_ALL, "Russian");
+    size_t letterReps = 0;
+    std::string phrase = "";
+    std::cout << "[IN]: ";
+    std::getline(std::cin, phrase);
+    std::set<char> setOfLetters;
+    std::multimap<size_t, char, std::greater<size_t>> mapOfLetters;
+
+    for (size_t i {}; i < phrase.length(); i++)
+    {
+        setOfLetters.insert(phrase[i]);
+    }
+
+    for (const auto& i : setOfLetters)
+    {
+        letterReps = std::count(phrase.begin(), phrase.end(), i);
+        mapOfLetters.emplace(letterReps, i);
+    }
+
+    std::cout << "[OUT]:" << std::endl;
+    for (const auto& pair : mapOfLetters)
+    {
+        std::cout << pair.second << ": " << pair.first << std::endl;
+    }
+    return 0;
+}
